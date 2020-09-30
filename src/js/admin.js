048773d57abce938ba936f6a898c5b8b8ad70b26
Ashen <avdunusinghe@gmail.com>
2020-09-30 22:39:52 +0530

    little changes

--- src/js/admin.js
+++ src/js/admin.js
@@ -5,3 +5,14 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+
+  function myFunction() {
+    var x = document.getElementById('myDIV'); 
+    //element will be invisible
+    if (x.style.visibility === 'hidden') {      
+      x.style.visibility = 'visible';
+    } else {
+      x.style.visibility = 'hidden';
+    }
+  }
