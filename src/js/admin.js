d54a37b67e5bd0e793fc1f13834cc56d60d9946f
Ashen <avdunusinghe@gmail.com>
2020-10-20 02:28:32 +0530

    adminpanel

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,18 +1,111 @@
-function OpenAdminPanel() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function claseAdminPanel() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+function validateAdminBooksUpdate(){
+
+    var inventoryId = document.getElementById("bookInvetoryId").value;
+    var bookTitle = document.getElementById("bookName").value;
+    var bookIsbn = document.getElementById("bookIsbn").value;
+    var bookPrice = document.getElementById("bookPrice").value;
+    var authorId = document.getElementById("bookAuthor").value;
+    var publicationId = document.getElementById("bookPublisherId").value;
+    var bookQuantity = document.getElementById("bookQuntity").value;
+    var bookImage = document.getElementById("BookImg").value;
 
 
-  function showAdminFunction() {
-    var x = document.getElementById('adminSetting'); 
-    //element invisible
-    if (x.style.visibility === 'hidden') {      
-      x.style.visibility = 'visible';
-    } else {
-      x.style.visibility = 'hidden';
-    }
+    if (inventoryId == null || inventoryId == "") {
+
+      alert("Please enter Inventory Id");
+      return false;
+
   }
+
+    if (bookTitle == null || bookTitle == "") {
+
+    alert("Please enter BookTitle");
+    return false;
+
+  }
+
+  if (bookIsbn == null || bookIsbn == "") {
+
+    alert("Please enter ISBN Number");
+    return false;
+
+  }
+
+  if (bookPrice == null || bookPrice == "") {
+
+    alert("Please enter Price");
+    return false;
+
+  }
+
+  if (authorId == null || authorId == "") {
+
+    alert("Please enter Author Id");
+    return false;
+
+  }
+
+  if (publicationId == null || publicationId == "") {
+
+    alert("Please enter Publisher Id");
+    return false;
+
+  }
+
+  if (bookQuantity == null || bookQuantity == "") {
+
+    alert("Please enter Publisher Id");
+    return false;
+
+  }
+
+  if (bookImage == null || bookImage == "") {
+
+    alert("Please Uplaod Book Image");
+    return false;
+
+  }
+
+  else {
+
+    alert('Operation Submited');
+
+}
+}
+
+function addAuthorValidation(){
+
+  var authorNameCheck = document.getElementById("BookAuthorName").value;
+
+
+  if (authorNameCheck == null || authorNameCheck == "") {
+
+    alert("Please Enter Author Name");
+    return false;
+
+  }
+
+  else {
+
+    alert('Operation Submited');
+
+}
+}
+
+function addPublisherValidate(){
+
+  var publisherNameCheck = document.getElementById("BookPubName").value;
+
+  if (publisherNameCheck == null || publisherNameCheck == "") {
+
+    alert("Please Enter Publisher Name");
+    return false;
+
+  }
+
+  else {
+
+    alert('Operation Submited');
+
+}
+}
