83a913d5c71c193edb6cb3192b87ff08ac042fd5
avdunusinghe <avdunusinghe@gmail.com>
2020-10-20 20:32:27 +0530

    ff

--- src/reqbook.php
+++ src/reqbook.php
@@ -1,13 +1,13 @@
 <?php
 
-include 'config.php'
+include 'config.php';
 
 $cName = $_POST["spname"];
 $cNumber = $_POST["sppnum"];
 $cEmail = $_POST["spemail"];
 $cBookDetails = $_POST["spbdetails"];
 
-$sql = "insert into requestbooks (ReqId ,Name, ContactNo, Email,BookReq) VALUES ('','$cName',$cNumber,'$cEmail', '$cBookDetails')";
+$sql = "insert into requestbooks (ReqId ,Name, ContactNo, Email,BookReq) VALUES ('','$cName',$cNumber,'$cEmail','$cBookDetails')";
 
 if($conn->query($sql)){
 
